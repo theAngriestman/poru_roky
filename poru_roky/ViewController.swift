f256449df61da74c6f4ad2129703f5b3504008e1
theAngriestman <yarik.lev7@gmail.com>
2021-12-06 19:45:58 +0200

    Initial Commit

---
+++ poru_roky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  poru_roky
+//
+//  Created by theAngriestman on 06.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
